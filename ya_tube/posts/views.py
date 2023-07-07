401e03558d6b18c39308d8e4cdbd69558a0bae4f
aeksei <aeksei94@gmail.com>
2023-07-07 14:22:48 +0300

    feat: добавлена логика возвращения списка постов

--- ya_tube/posts/views.py
+++ ya_tube/posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+@api_view(["GET", "POST"])
+def api_posts(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
