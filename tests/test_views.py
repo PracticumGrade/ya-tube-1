c8a77d5caa149f89d8be88dac828f50598e6dfad
aeksei <aeksei94@gmail.com>
2023-07-07 18:43:39 +0300

    test: перемещены тесты

--- tests/test_views.py
+++ tests/test_views.py
@@ -30,61 +30,6 @@ def post_pk_for_args(post):
     return post.pk,
 
 
-def test_posts_list(api_client, posts):
-    url = reverse("posts:posts-list")
-    response = api_client.get(url)
-
-    data = response.json()
-
-    assert isinstance(data, list), (
-        f"Убедитесь, что при отправке GET-запроса на url `{url}`  "
-        f"возвращается список."
-    )
-    assert len(data) == N_PER_FIXTURE, (
-        f"Убедитесь, что при отправке GET-запроса на url `{url}`  "
-        f"для получение списка постов возвращаются все посты."
-    )
-
-
-def test_create_post(api_client, post_create_data):
-    url = reverse("posts:posts-list")
-    response = api_client.post(url, data=post_create_data)
-
-    assert Post.objects.count() == 1, (
-        f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
-        f"для создания нового поста, объект был добавлен в БД."
-    )
-
-    data = response.json()
-    assert isinstance(data, dict), (
-        f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
-        f"для создания нового поста возвращается словарь"
-    )
-
-    for field in POST_FIELDS:
-        assert field in data, (
-            f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
-            f"для создания нового поста, возвращаемый словарь содержит поле `{field}`"
-        )
-
-    text_field = "text"
-    assert post_create_data[text_field] == data[text_field], (
-        f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
-        f"для создания нового поста, возвращается словарь созданного объекта с заполненным полем `{text_field}`"
-    )
-
-
-def test_incorrect_create_post(api_client):
-    url = reverse("posts:posts-list")
-    empty_data = {}
-    response = api_client.post(url, data=empty_data)
-
-    assert Post.objects.count() == 0, (
-        f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
-        f"для создания нового поста, объект не был добавлен в БД."
-    )
-
-
 @pytest.mark.parametrize(
     "method,name,args,status_code,data", [
         ("GET", "posts:posts-list", None, status.HTTP_200_OK, None,),
@@ -149,4 +94,59 @@ def test_bad_request_and_errors(api_client, post, method, name, args):
     assert data == expected_value, (
         f"Убедитесь, что при отправке {method}-запроса на url `{url}`  "
         f"c некорректными данными, в теле ответа возвращаются ошибки."
+    )
+
+
+def test_posts_list(api_client, posts):
+    url = reverse("posts:posts-list")
+    response = api_client.get(url)
+
+    data = response.json()
+
+    assert isinstance(data, list), (
+        f"Убедитесь, что при отправке GET-запроса на url `{url}`  "
+        f"возвращается список."
+    )
+    assert len(data) == N_PER_FIXTURE, (
+        f"Убедитесь, что при отправке GET-запроса на url `{url}`  "
+        f"для получение списка постов возвращаются все посты."
+    )
+
+
+def test_create_post(api_client, post_create_data):
+    url = reverse("posts:posts-list")
+    response = api_client.post(url, data=post_create_data)
+
+    assert Post.objects.count() == 1, (
+        f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
+        f"для создания нового поста, объект был добавлен в БД."
+    )
+
+    data = response.json()
+    assert isinstance(data, dict), (
+        f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
+        f"для создания нового поста возвращается словарь"
+    )
+
+    for field in POST_FIELDS:
+        assert field in data, (
+            f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
+            f"для создания нового поста, возвращаемый словарь содержит поле `{field}`"
+        )
+
+    text_field = "text"
+    assert post_create_data[text_field] == data[text_field], (
+        f"Убедитесь, что при отправке POST-запроса на url `{url}`  "
+        f"для создания нового поста, возвращается словарь созданного объекта с заполненным полем `{text_field}`"
+    )
+
+
+def test_incorrect_create_post(api_client):
+    url = reverse("posts:posts-list")
+    empty_data = {}
+    api_client.post(url, data=empty_data)
+
+    assert Post.objects.count() == 0, (
+        f"Убедитесь, что при отправке POST-запроса с некорректными данными на url `{url}`  "
+        f"для создания нового поста, объект не был добавлен в БД."
     )
